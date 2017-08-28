f7a13344696a3b6bb80c106551769ee724e2868c
Stefan Schliebs <stefan.schliebs@qrious.co.nz>
2017-08-28 23:02:18 +1200

    Network graph prep added

--- data/prep_network.R
+++ data/prep_network.R
@@ -18,16 +18,27 @@ d <- read_rds("data/eye-gene-data.rds") %>%
   select(`Gene Symbol`, Phenotypes) %>% 
   distinct()
 
+
+# Create network ----------------------------------------------------------
+
+# all genes that have the same phenotype will be connected 
 trans <- as(split(d$`Gene Symbol`, d$Phenotypes), "transactions")
+
+# convert transactions into adjacency matrix which can be loaded into igraph
 adj_mat <- crossTable(trans, measure = "count")
 
-
+# create igraph
 g <- graph.adjacency(adj_mat, mode = "undirected", weighted = TRUE)
+
+# compute node groups
 wc <- cluster_walktrap(g)
 members <- membership(wc)
 
+# convert igraph into networkD3 graph
 gd3 <- igraph_to_networkD3(g, group = members)
 
+# visualise graph
+# NOTE: This is super slow...
 forceNetwork(Links = gd3$links, Nodes = gd3$nodes, 
              Source = 'source', Target = 'target', 
              NodeID = 'name', Group = 'group')
